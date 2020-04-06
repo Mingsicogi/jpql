bc9da2b45b0f5c616821f8ee45039d0b3cc48161
Mingsicogi <codedoctor119@gmail.com>
2020-04-06 23:50:06 +0900

    + JPQL Function 기본 함수 예제 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -149,6 +149,8 @@ public class Main {
         List<String> singleResult = em.createQuery("select concat('aaa', 'bbb', 'cccc') from Member m", String.class).getResultList();
         singleResult.forEach(System.out::println);
 
+
+
         tx.commit();
         em.close();
         emf.close();
